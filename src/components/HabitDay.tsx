99dde314542dd82e71c669e8217a9b9254b48d6e
Guilherme Bafica <gb.engcomp@hotmail.com>
2023-02-14 21:42:42 -0300

    Finalize popover day detail

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React from "react";
+import * as Popover from "@radix-ui/react-popover";
+
+import { ProgressBar } from "./ProgressBar";
 
 // interface IHabitDayProps {
 //   completed: number;
@@ -6,6 +9,21 @@ import React from 'react';
 
 export function HabitDay() {
   return (
-    <div className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg' />
-  )
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">
+            17/01
+          </span>
+
+          <ProgressBar progress={75} />
+
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  );
 }
---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface IProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar(props: IProgressBarProps) {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={props.progress}
+        className="h-3 rounded-xl bg-violet-600"
+        style={{ width: `${props.progress}%` }}
+      />
+    </div>
+  );
+}
