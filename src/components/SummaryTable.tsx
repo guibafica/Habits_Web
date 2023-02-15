f1572e9f2157916299e659bd358361d226ca2a33
Guilherme Bafica <gb.engcomp@hotmail.com>
2023-02-14 22:01:07 -0300

    finalize the habitDay dinamic color

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 
-import { HabitDay } from './HabitDay';
+import { HabitDay } from "./HabitDay";
 
 export function SummaryTable() {
-  const daysArray = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+  const daysArray = ["D", "S", "T", "Q", "Q", "S", "S"];
 
   const summaryDates = generateDatesFromYearBeginning();
 
@@ -13,29 +13,34 @@ export function SummaryTable() {
   const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
   return (
-    <div className='w-full flex'>
-      <div className='grid grid-rows-7 grid-flow-row gap-3'>
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
         {daysArray.map((day, dayIndex) => (
           <div
             key={`${dayIndex} - ${day}`}
-            className='text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center'
+            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
           >
             {day}
           </div>
         ))}
       </div>
 
-      <div className='grid grid-rows-7 grid-flow-col gap-3'>
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => (
-          <HabitDay key={date.toString()} />
-        ))}
-
-        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => (
-          <div
-            key={i}
-            className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed'
+          <HabitDay
+            key={date.toString()}
+            amount={5}
+            completed={Math.round(Math.random() * 5)}
           />
         ))}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+            <div
+              key={i}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
       </div>
     </div>
   );
